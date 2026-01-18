4819f4c8bed941ec7a8fe48bac4b6f2480da5503
Pedro H. <pedro_gonsalves@hotmail.com>
2026-01-18 00:58:10 -0300

    add: function for send any type of file

--- external/evolution/__init__.py
+++ external/evolution/__init__.py
@@ -3,4 +3,4 @@ from external.evolution.message import send_message
 from external.evolution.audio import send_audio
 from external.evolution.image import send_sticker, send_animated_sticker, extract_quoted_image_bytes, send_image, get_profile_info
 from external.evolution.base import evolution_instance_key
-from external.evolution.media import download_media
+from external.evolution.media import download_media, send_media
--- external/evolution/media.py
+++ external/evolution/media.py
@@ -1,3 +1,5 @@
+import base64
+
 import httpx
 
 from external.evolution.base import evolution_instance_name, evolution_api_key, evolution_api
@@ -28,3 +30,27 @@ async def download_media(message_id: str, convert_to_mp4: bool = False) -> tuple
 
         if 'base64' in result:
             return result["base64"], result["fileName"]
+
+
+async def send_media(contact_id: str, file_path: str):
+    media_url = f"{evolution_api}/message/sendMedia/{evolution_instance_name}"
+    with open(file_path, "rb") as f:
+        file_data = base64.b64encode(f.read()).decode("utf-8")
+
+    payload = {
+        "number": contact_id,
+        "media": file_data,
+        "mimetype": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", # TODO: add a map with file extension -> mimetype
+        "fileName":file_path.split("/")[-1],
+        "caption": "Tá aqui o arquivo solicitado. Qualquer outra coisa pode me chamar",
+        "mediatype": "document"
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "apikey": evolution_api_key
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(media_url, json=payload, headers=headers, timeout=30)
+        response.raise_for_status()
